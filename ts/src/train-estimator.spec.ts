a6e69c2129ceebc5822fdef1175d0ba30e740bfd
Hugo Huzé--Cardosi <hugo.cardosi@hotmail.fr>
2024-05-03 14:27:55 +0200

    test: prise en main du kata

--- ts/src/train-estimator.spec.ts
+++ ts/src/train-estimator.spec.ts
@@ -1,6 +1,16 @@
+import {TrainTicketEstimator} from "./train-estimator";
+import {Passenger, TripDetails, TripRequest} from "./model/trip.request";
 
 describe("train estimator", function () {
-    it("should work", () => {
-        expect(1 + 2).toBe(3);
+    it('should estimate a train ticket', function () {
+        // ARRANGE
+        const trainTicketEstimator = new TrainTicketEstimator();
+        const tripDetails = new TripDetails("Paris", "Lyon", new Date('2024-06-01'));
+        const passengers: Passenger[] = [];
+        const tripRequest: TripRequest = {details: tripDetails, passengers: passengers};
+        // ACT
+        const estimation = trainTicketEstimator.estimate(tripRequest);
+        // ASSERT
+        expect(estimation).toBeGreaterThan(0);
     });
 });
