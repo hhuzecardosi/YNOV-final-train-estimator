567dc31c73ee22397680f1a057b097658ada93c8
Hugo Huzé--Cardosi <hugo.cardosi@hotmail.fr>
2024-05-03 14:30:38 +0200

    test: oublie de l'asynchrone

--- ts/src/train-estimator.spec.ts
+++ ts/src/train-estimator.spec.ts
@@ -2,15 +2,15 @@ import {TrainTicketEstimator} from "./train-estimator";
 import {Passenger, TripDetails, TripRequest} from "./model/trip.request";
 
 describe("train estimator", function () {
-    it('should estimate a train ticket', function () {
+    it('should estimate a train ticket', async function () {
         // ARRANGE
         const trainTicketEstimator = new TrainTicketEstimator();
         const tripDetails = new TripDetails("Paris", "Lyon", new Date('2024-06-01'));
         const passengers: Passenger[] = [];
         const tripRequest: TripRequest = {details: tripDetails, passengers: passengers};
         // ACT
-        const estimation = trainTicketEstimator.estimate(tripRequest);
+        const estimation = await trainTicketEstimator.estimate(tripRequest);
         // ASSERT
-        expect(estimation).toBeGreaterThan(0);
+        expect(estimation).toEqual(0);
     });
 });
